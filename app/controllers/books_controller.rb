1c406c510a39a5190593c61030cea1e1bc82cee0
EC2 Default User <kazukipump7@gmail.com>
2022-07-05 07:29:35 +0000

    finish-7c

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,17 +8,28 @@ class BooksController < ApplicationController
     @user = @book.user
   end
 
+
   def index
-    to  = Time.current.at_end_of_day
-    from  = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).
-      sort {|a,b|
-        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
-        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+      to  = Time.current.at_end_of_day
+      from  = (to - 6.day).at_beginning_of_day
+      @books = Book.all.sort {|a,b|
+        b.favorites.where(created_at: from...to).size <=>
+        a.favorites.where(created_at: from...to).size
       }
-    @book = Book.new
-    # @books = Book.all
+      @book = Book.new
   end
+
+  # def index
+  #   to  = Time.current.at_end_of_day
+  #   from  = (to - 6.day).at_beginning_of_day
+  #   @books = Book.includes(:favorited_users).
+  #     sort {|a,b|
+  #       b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+  #       a.favorited_users.includes(:favorites).where(created_at: from...to).size
+  #     }
+  #   @book = Book.new
+  #   # @books = Book.all
+  # end
 
   def create
     @book = Book.new(book_params)
